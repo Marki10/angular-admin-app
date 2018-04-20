5e38604db29351ba684759edbfd2c9cfdc942c29
Marki10 <markdarabos@gmail.com>
2018-04-20 13:04:06 +0200

    Add data, update style

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
   menuItems = [
-    { title: 'Home', page: '', icon: 'bandcamp'},
-    { title: 'Style', page: 'style', icon: 'bandcamp'},
-    { title: 'Options', page: 'options', icon: 'address-book' },
-    { title: 'Usage', page: 'usage', icon: 'address-book' },
-    { title: 'Feedback', page: 'feedback', icon: 'archive' }
+    { title: 'Home', page: '', icon: 'bandcamp', newNotification: 3 },
+    { title: 'Style', page: 'style', icon: 'bandcamp', newNotification: 0 },
+    { title: 'Options', page: 'options', icon: 'archive', newNotification: 0 },
+    { title: 'Usage', page: 'usage', icon: 'address-book', newNotification: 1 },
+    { title: 'Feedback', page: 'feedback', icon: 'chart-pie', newNotification: 8 }
   ];
   
   constructor() {
